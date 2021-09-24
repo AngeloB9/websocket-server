eaf5df04e3897e668d32b08b782befe7db5de00e
AngeloBenavidez <benavidezangelo9@gmail.com>
2021-09-24 11:09:43 -0500

    Socket.io add

--- models/server.js
+++ models/server.js
@@ -5,6 +5,8 @@ class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+    this.server = require('http').createServer(this.app);
+    this.io = require('socket.io')(this.server);
 
     this.paths = {};
 
@@ -13,6 +15,9 @@ class Server {
 
     // Rutas de mi aplicación
     this.routes();
+
+    //Sockets
+    this.sockets();
   }
 
   middlewares() {
@@ -27,8 +32,17 @@ class Server {
     //this.app.use(this.paths.auth, require('../routes/auth'));
   }
 
+  sockets() {
+    this.io.on('connection', (socket) => {
+      console.log('Cliente conectado', socket.id);
+      socket.on('disconnect', () => {
+        console.log('Cliente desconectado', socket.id);
+      });
+    });
+  }
+
   listen() {
-    this.app.listen(this.port, () => {
+    this.server.listen(this.port, () => {
       console.log('Servidor corriendo en puerto', this.port);
     });
   }
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,18 @@
-console.log('Hola mundo');
+//Referencias del html
+
+const lblonline = document.querySelector('#lblonline');
+const lbloffline = document.querySelector('#lbloffline');
+
+const socket = io();
+
+socket.on('connect', () => {
+  console.log('Conectado');
+  lbloffline.style.display = 'none';
+  lblonline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+  console.log('Desconectado del servidor');
+  lblonline.style.display = 'none';
+  lbloffline.style.display = '';
+});
